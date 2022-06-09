94f02ee73479e7ed001ee973147433732933b862
mendesleu <leonardo.mendes10@hotmail.com>
2022-06-08 21:23:45 -0300

    Repetições

---
+++ Curso PHP - Udemy/Repeticoes/desafio_impressao.php
@@ -0,0 +1,23 @@
+<div class="titulo">Desafio Impressão</div>
+
+<?php
+$array = [
+    "AAA",
+    "BBB",
+    "CCC",
+    "DDD",
+    "EEE",
+    "FFF"
+];
+
+for($i = 0; $i < count($array); $i++){
+    if($i % 2 === 1) continue;
+    echo "{$array[$i]} <br>";
+}
+
+echo "<hr>";
+
+foreach($array as $chave => $valor){
+    if($chave % 2 !== 0) continue;
+    echo "$valor <br>";
+}
--- Curso PHP - Udemy/index.php
+++ Curso PHP - Udemy/index.php
@@ -28,6 +28,12 @@
                     <ul>
                         <li><a href="exercicio.php?dir=repeticoes&file=for">Laço For</a></li>
                         <li><a href="exercicio.php?dir=repeticoes&file=desafio_for">Desafio For</a></li>
+                        <li><a href="exercicio.php?dir=repeticoes&file=foreach">Foreach</a></li>
+                        <li><a href="exercicio.php?dir=repeticoes&file=break_continue">Break/Continue</a></li>
+                        <li><a href="exercicio.php?dir=repeticoes&file=desafio_impressao">Desafio Impressão</a></li>
+                        <li><a href="exercicio.php?dir=repeticoes&file=while">While/DO While</a></li>
+                        <li><a href="exercicio.php?dir=repeticoes&file=desafio_tabela">Desafio Tabela #01</a></li>
+                        <li><a href="exercicio.php?dir=repeticoes&file=desafio_tabela2">Desafio Tabela #02</a></li>
                     </ul>
                 </div>
                 <div class="modulo laranja">
